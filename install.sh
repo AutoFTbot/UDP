12650f71b532f2a295624b484ce74ffc34643652
AutoFTbot <151357338+AutoFTbot@users.noreply.github.com>
2024-08-10 14:26:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+
+# Update dan upgrade
 apt update -y
 apt upgrade -y
+
+# Instalasi paket
 apt install lolcat -y
 apt install figlet -y
 apt install neofetch -y
 apt install screenfetch -y
+
+# Setup direktori
 cd
 rm -rf /root/udp
 mkdir -p /root/udp
 
-# banner
+# Banner
 clear
 echo -e "                _        ______ _   ____        _   " | lolcat
 echo -e "     /\\        | |      |  ____| | |  _ \\      | |  " | lolcat
@@ -21,26 +27,25 @@ echo -e "                                                    " | lolcat
 echo ""
 echo ""
 sleep 5
-# change to time GMT+5:30
 
+# Ubah zona waktu
 echo "Changing time zone to GMT+7:00 Jakarta"
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
-
-
-# install udp-custom
-echo downloading udp-custom
+# Instalasi udp-custom
+echo "Downloading udp-custom"
 wget "https://github.com/AutoFTbot/UDP/raw/main/udp-custom-linux-amd64" -O /root/udp/udp-custom
 chmod +x /root/udp/udp-custom
 
-echo downloading default config
+echo "Downloading default config"
 wget "https://raw.githubusercontent.com/AutoFTbot/UDP/main/config.json" -O /root/udp/config.json
 chmod 644 /root/udp/config.json
 
+# Buat file service
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom by ePro Dev. Team and modify by AutoFtBot
+Description=UDP Custom by ePro Dev. Team and modified by AutoFtBot
 
 [Service]
 User=root
@@ -56,7 +61,7 @@ EOF
 else
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom by ePro Dev. Team and modify by AutoFtBot
+Description=UDP Custom by ePro Dev. Team and modified by AutoFtBot
 
 [Service]
 User=root
@@ -78,14 +83,15 @@ echo ''
 echo ''
 echo ''
 sleep 5
+
+# Instalasi sistem tambahan
 cd $HOME
-mkdir /etc/AutoFtBot
+mkdir -p /etc/AutoFtBot
 cd /etc/AutoFtBot
 wget https://github.com/AutoFTbot/UDP/raw/main/system.zip
-unzip system
+unzip system.zip
 cd /etc/AutoFtBot/system
 mv menu /usr/local/bin
-cd /etc/AutoFtBot/system
 chmod +x ChangeUser.sh
 chmod +x Adduser.sh
 chmod +x DelUser.sh
@@ -97,7 +103,6 @@ chmod +x menu
 cd /etc/AutoFtBot
 rm system.zip
 
-
 clear
 echo 'UDP Install Script By Project AutoFTbot Dev.Team'
 echo 'UDP Custom By ePro Dev. Team'
@@ -108,11 +113,13 @@ echo "Github/AutoFTbot"
 echo "Telegram/AutoFTbot"
 sleep 5
 
-echo start service udp-custom
+# Mulai dan aktifkan layanan
+echo "Starting udp-custom service"
 systemctl start udp-custom &>/dev/null
 
-echo enable service udp-custom
+echo "Enabling udp-custom service"
 systemctl enable udp-custom &>/dev/null
 
-echo reboot
+# Reboot sistem
+echo "Rebooting system"
 reboot
